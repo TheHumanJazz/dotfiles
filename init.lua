b5dd6b51e36701e4c831fefbf440846a896244ec
Jared Phillis <jared.phillis@vivanti.com>
2025-04-20 07:38:05 +1000

    update

--- init.lua
+++ init.lua
@@ -13,142 +13,211 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local spec = {
-    {
-        "sainnhe/gruvbox-material",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.g.gruvbox_material_enable_italic = true
-            vim.cmd.colorscheme('gruvbox-material')
-        end,
-    },
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        opts = {
-            options = {
-                icons_enabled = true,
-                theme = 'gruvbox-material',
-                component_separators = { left = '', right = ''},
-                section_separators = { left = '', right = ''},
-                disabled_filetypes = {
-                  statusline = {},
-                  winbar = {},
-                },
-                ignore_focus = {},
-                always_divide_middle = true,
-                always_show_tabline = true,
-                globalstatus = false,
-                refresh = {
-                  statusline = 100,
-                  tabline = 100,
-                  winbar = 100,
-                }
-              },
-              sections = {
-                lualine_a = {'mode'},
-                lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filename'},
-                lualine_x = {'encoding', 'fileformat', 'filetype'},
-                lualine_y = {'progress'},
-                lualine_z = {'location'}
-              },
-              inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = {'filename'},
-                lualine_x = {'location'},
-                lualine_y = {},
-                lualine_z = {}
-              },
-              tabline = {},
-              winbar = {},
-              inactive_winbar = {},
-              extensions = {}
-        },
-    },
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            "junegunn/fzf",
-        },
-        tag = '0.1.8',
-        lazy = false,
-        opts = {
-            highlight = {
-                enable = true,
-            },
-        },
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        event = { "BufReadPost", "BufNewFile" },
-        opts = {
-            highlight = {
-                enable = true,
-            },
-            auto_installed = true,
-        }
-    },
-    {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        config = true,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/nvim-cmp",
-            'hrsh7th/cmp-cmdline',
-            'neovim/nvim-lspconfig',
-            'hrsh7th/cmp-nvim-lsp',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-path',
-            'hrsh7th/cmp-cmdline',
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
-            'onsails/lspkind.nvim',
-        }
-    },
-    {
-        "kristijanhusak/vim-dadbod-ui",
-        dependencies = {
-            -- "tpope/vim-dadbod",
-            -- "kristijanhusak/vim-dadbod-completion",
-            { 'tpope/vim-dadbod', lazy = true },
-            { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
-        },
-        cmd = {
-            'DBUI',
-            'DBUIToggle',
-            'DBUIAddConnection',
-            'DBUIFindBuffer',
-        },
-        init = function()
-            -- Your DBUI configuration
-            vim.g.db_ui_use_nerd_fonts = 1
-        end,
-    },
-    {
-        "christoomey/vim-tmux-navigator",
-    },
-    {
-        "numToStr/Comment.nvim",
-    },
-    {
-        "tpope/vim-fugitive"
-    }
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.g.gruvbox_material_enable_italic = true
+			vim.cmd.colorscheme("gruvbox-material")
+		end,
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^5", -- Recommended
+		lazy = false, -- This plugin is already lazy
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		opts = {
+			options = {
+				icons_enabled = true,
+				theme = "gruvbox-material",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				always_show_tabline = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 100,
+					tabline = 100,
+					winbar = 100,
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			winbar = {},
+			inactive_winbar = {},
+			extensions = {},
+		},
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"junegunn/fzf",
+		},
+		tag = "0.1.8",
+		lazy = false,
+		opts = {
+			highlight = {
+				enable = true,
+			},
+		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = { "BufReadPost", "BufNewFile" },
+		opts = {
+			highlight = {
+				enable = true,
+			},
+			auto_installed = true,
+		},
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/nvim-cmp",
+			"hrsh7th/cmp-cmdline",
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-cmdline",
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"onsails/lspkind.nvim",
+		},
+	},
+	{
+		"kristijanhusak/vim-dadbod-ui",
+		dependencies = {
+			-- "tpope/vim-dadbod",
+			-- "kristijanhusak/vim-dadbod-completion",
+			{ "tpope/vim-dadbod", lazy = true },
+			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+		},
+		cmd = {
+			"DBUI",
+			"DBUIToggle",
+			"DBUIAddConnection",
+			"DBUIFindBuffer",
+		},
+		init = function()
+			-- Your DBUI configuration
+			vim.g.db_ui_use_nerd_fonts = 1
+		end,
+	},
+	{
+		"christoomey/vim-tmux-navigator",
+	},
+	{
+		"numToStr/Comment.nvim",
+	},
+	-- { "nvim-java/nvim-java" },
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {},
+	},
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+		opts = {
+			-- add any opts here
+			-- for example
+			provider = "openai",
+			openai = {
+				endpoint = "https://api.openai.com/v1",
+				model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+				timeout = 30000, -- timeout in milliseconds
+				temperature = 0, -- adjust if needed
+				max_tokens = 10000,
+				-- reasoning_effort = "high" -- only supported for reasoning models (o1, etc.)
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	},
 }
 
 require("lazy").setup({
-    spec = spec,
-    install = { colorscheme = { "gruvbox-material" }},
-    checker = { enabled = true },
+	spec = spec,
+	install = { colorscheme = { "gruvbox-material" } },
+	checker = { enabled = true },
 })
-
 
 -- setup lsp
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -156,7 +225,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 	callback = function(ev)
 		-- Enable completion triggered by <c-x><c-o>
 		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-        vim.cmd("TSEnable highlight")
+		vim.cmd("TSEnable highlight")
 
 		-- Buffer local mappings.
 		-- See `:help vim.lsp.*` for documentation on any of the below functions
@@ -184,8 +253,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
 local capabilities_with_completion = require("cmp_nvim_lsp").default_capabilities()
 capabilities_with_completion.textDocument.completion.completionItem.snippetSupport = true
 
-require ('lspconfig').util.default_config.on_init = function(client, _)
-  client.server_capabilities.semanticTokensProvider = nil
+require("lspconfig").util.default_config.on_init = function(client, _)
+	client.server_capabilities.semanticTokensProvider = nil
 end
 
 require("mason").setup()
@@ -219,7 +288,7 @@ require("mason-lspconfig").setup_handlers({
 					analyses = {
 						unusedparams = true,
 						shadow = true,
-						fieldalignment = true,
+						-- fieldalignment = true,
 						nilness = true,
 						unusedwrite = true,
 					},
@@ -275,10 +344,24 @@ require("mason-lspconfig").setup_handlers({
 	["ts_ls"] = function()
 		require("lspconfig").ts_ls.setup({})
 	end,
-	["csharp_ls"] = function()
-		require("lspconfig").omnisharp.setup({})
+	-- ["csharp_ls"] = function()
+	-- 	require("lspconfig").omnisharp.setup({})
+	-- end,
+	["buf_ls"] = function()
+		require("lspconfig").buf_ls.setup({})
 	end,
+	["java_language_server"] = function()
+		require("lspconfig").java_language_server.setup({})
+	end,
+	-- ["clangd"] = function()
+	-- 	require("lspconfig").clangd.setup({
+	--            cmd = { "clangd", "--std=c++11" },
+	--        })
+	-- end,
 })
+
+-- require("java").setup()
+-- require("lspconfig").jdtls.setup({})
 
 -- LSP Diagnostics Options Setup
 local sign = function(opts)
@@ -346,7 +429,7 @@ cmp.setup({
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 		end,
 	},
 	window = {
@@ -371,8 +454,8 @@ cmp.setup({
 		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif vim.fn["vsnip#available"](1) == 1 then
-				feedkey("<Plug>(vsnip-expand-or-jump)", "")
+			-- elseif vim.fn["vsnip#available"](1) == 1 then
+			-- 	feedkey("<Plug>(vsnip-expand-or-jump)", "")
 			elseif has_words_before() then
 				cmp.complete()
 			else
@@ -415,6 +498,38 @@ cmp.setup.cmdline(":", {
 		{ name = "cmdline" },
 	}),
 })
+-- conform
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "isort", "black" },
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		rust = { "rustfmt", lsp_format = "fallback" },
+		-- Conform will run the first available formatter
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		typescript = { "prettierd", stop_after_first = true },
+		typescriptreact = { "prettierd", stop_after_first = true },
+		jsx = { "prettierd", stop_after_first = true },
+		html = { "prettierd", stop_after_first = true },
+		css = { "prettierd", stop_after_first = true },
+		json = { "prettierd", stop_after_first = true },
+		yaml = { "prettierd", stop_after_first = true },
+		sh = { "beautysh", stop_after_first = true },
+		bash = { "beautysh", stop_after_first = true },
+		go = { "goimports", "gofumpt", "golines", stop_after_first = false },
+		haskell = { "fourmolu", stop_after_first = true },
+		java = { "google-java-format", stop_after_first = true },
+	},
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
+})
+
 -- setup keymaps
 vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
@@ -438,7 +553,7 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fb", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fs", function()
-    builtin.grep_string({ search = vim.fn.input("Grep >") })
+	builtin.grep_string({ search = vim.fn.input("Grep >") })
 end)
 
 --
@@ -456,7 +571,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.api.nvim_create_autocmd("FileType", {
-	pattern = "c",
+	pattern = { "c", "typescript", "javascript", "json", "html", "typescriptreact", "css", "terraform", "java" },
 	callback = function()
 		vim.opt_local.shiftwidth = 2
 		vim.opt_local.tabstop = 2
@@ -480,4 +595,3 @@ vim.opt.incsearch = true
 vim.opt.scrolloff = 8
 
 vim.opt.colorcolumn = "80"
-
